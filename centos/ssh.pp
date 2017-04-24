a9994a7dd36b05aea821be49a5a6803c2a24c133
Bert Bullough <bbullough@savvysherpa.com>
2017-04-24 14:54:25 -0600

    Delete ssh.pp

--- centos/ssh.pp
+++
@@ -1,6 +0,0 @@
-ssh_athorized_key { 'centos@centos7':
-  user => 'centos',
-  type => 'ssh-rsa',
-  key  => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/UYrEady0sAAwHKutCArFVsLTsUtVtycQb78toFkymqMLDb92FHG68Y72zj+6WfKB40GyqlOGdx/lCOY64R4/O4/Vl3AQfNtQd1Bbs0IuSIuN8eM6AiSrxlODzne14/NZvAEklX6/FJfMmsxVOegH0RMR2T8e+DgehfVMQ/Tbpy/EnyxiyF9wdovhukf5+3lnYb+5KoDHd/xH88/x7UYvR5fAf+UkGETlvUrxAQ/XGWHvY9AKf1mGFWL7WvnEjkr6trvQ68gzH8U7ova1SKOwRVm5IPM4b/Q4rRpL3sap7o77ejxJrTfK7Eept/aoqekKstLBzhwcdaUxoTokgdr1 centos@centos7.bertbullough.com', 
-}
-
