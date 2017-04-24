3212a717efccd7e627bdc1bcdd5e508938148cdd
dirtmerchant <bert@bertbullough.com>
2017-04-24 15:04:44 -0600

    ssh.pp

---
+++ centos/ssh.pp
@@ -0,0 +1,5 @@
+ssh_authorized_key { 'centos@centos7':
+  user => 'tux',
+  type => 'ssh-rsa',
+  key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC/CeP+TPHpxxxrov2jDkqLGFMirIAAkJTnH3WiAfF8vjAdqXfnOnq43NDFbwi55YfNDzmB7jbQ/EJbVHtN8FgDuf0hf6CN1cF8M4Hp5mt1Oea+Ffx04ZPAXqiqxQzcAIgK6BkJwkNG72ed7aVFJHMFgLPckb+0CjhCETzBEQIabhVdeLlgl6F9PYUksrXFuF1b7+VZkYjLY+0ehOs3Hv/DvNiT/TKp9xZamzcFRmKD0sHHmdZpWVoQiO2viAwfbxa5kFRkyxd/Mummuy2d9Zw/CEdRWAd06YZ2JhOUzUEYWemZocOYoQr8dtyZSSdTJelcRwMvSdzGRKBsPovDLM0D',
+}
