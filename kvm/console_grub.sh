f8a6300f8b8c04e21d7b174de515293aa096c9f8
Bert Bullough <bert@bertbullough.com>
2017-05-23 14:10:32 -0600

    Create console_grub.sh

---
+++ kvm/console_grub.sh
@@ -0,0 +1,3 @@
+ #/bin/bash
+ 
+ sed -i 's|^GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"|GRUB_CMDLINE_LINUX_DEFAULT="quiet splash console=ttyS0,38400n8 console=tty0"|' /etc/default/grub && sudo update-grub
